fc58f56c20afeda866e5fda87e7bbfeeb2cf4cb7
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-22 23:35:21 -0600

    created function and route to get a single review by its id

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -14,6 +14,30 @@ const getAllReviews = async (req, res) => {
   }
 };
 
+const getReview = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      res.status(400).json('Must use a valid review id to find a review');
+    }
+
+    const urlId = new ObjectId(req.params.id);
+
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('reviews')
+      .find({ _id: urlId });
+
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   getAllReviews,
+  getReview,
 };
--- routes/reviews.js
+++ routes/reviews.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const reviewsController = require('../controllers/reviews');
 
 router.get('/', reviewsController.getAllReviews);
+router.get('/:id', reviewsController.getReview);
 
 module.exports = router;
