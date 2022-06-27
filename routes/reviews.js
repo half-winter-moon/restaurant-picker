fce2cd1ac6dc54b6cd2cbb0bd3e6e21254210c3b
Kevin <kevin.castro.chavez0@gmail.com>
2022-06-27 11:59:08 -0600

    created validation functions for creating and saving restaurants and reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const reviewsController = require('../controllers/reviews');
+const validator = require('../validation-middleware');
 
 router.get('/', reviewsController.getAllReviews);
 router.get('/:id', reviewsController.getReview);
-router.post('/', reviewsController.createReview);
-router.put('/:id', reviewsController.updateReview);
+router.post('/', validator.saveReview, reviewsController.createReview);
+router.put('/:id', validator.saveReview, reviewsController.updateReview);
 router.delete('/:id', reviewsController.deleteReview);
 
 module.exports = router;
--- validation-middleware.js
+++ validation-middleware.js
@@ -1,5 +1,49 @@
 const validator = require('./validate.js');
 
+const saveRestaurant = (req, res, next) => {
+  const validationRule = {
+    restaurantName: 'required|string',
+    cuisine: 'required|ObjectId',
+    description: 'required|string',
+    address: 'required|string',
+    zipCode: 'required|string',
+    opens: 'required|string',
+    closes: 'required|string',
+    phoneNumber: 'required|string',
+    restaurantWebsite: 'required|string',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Please add the missing fields or match the correct type',
+        data: err,
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+const saveReview = (req, res, next) => {
+  const validationRule = {
+    postedBy: 'required|ObjectId',
+    restaurantId: 'required|ObjectId',
+    content: 'required|string',
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Please add the missing fields or match the correct type',
+        data: err,
+      });
+    } else {
+      next();
+    }
+  });
+};
+
 const saveCuisine = (req, res, next) => {
   const validationRule = {
     name: 'required|string',
@@ -38,6 +82,8 @@ const saveUser = (req, res, next) => {
 };
 
 module.exports = {
+  saveRestaurant,
+  saveReview,
   saveCuisine,
   saveUser,
 };
